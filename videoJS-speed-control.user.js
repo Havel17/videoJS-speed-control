6f521726e109ff3d07f7bff21cfc8cbc7431c43c
Havel17 <abramchukdaniil@gmail.com>
2025-01-27 14:31:28 +0300

    2

--- videoJS-speed-control.user.js
+++ videoJS-speed-control.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         videoJS speed control
 // @namespace    http://tampermonkey.net/
-// @version      2024-11-28
+// @version      1.1.0
 // @description  SPEED ETO SMESHNO
 // @author       Havel
 // @match        https://*.tiktok.com/*
