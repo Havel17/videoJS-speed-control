cac88120b2b36804aee798c2bc9f636979d32128
Havel17 <abramchukdaniil@gmail.com>
2025-01-28 14:22:55 +0300

    1

--- videoJS-speed-control.user.js
+++ videoJS-speed-control.user.js
@@ -116,8 +116,11 @@ var addSpeedToParent = function() {
 }
 
 var checkAudioSpeed = function () {
-
-    $('body').attr("style", "background-image: none !important")
+    document.cookie.split(';').forEach(element => {
+        if (element.split('=')[0] == 'dle_user_id') {
+            $('body').attr("style", "background-image: none !important")
+       }
+    });
 
     if (document.querySelector('video') && document.querySelector('video').playbackRate != 1) {
         document.querySelector('video').playbackRate = 1;
